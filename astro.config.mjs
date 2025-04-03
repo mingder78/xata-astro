5e2edba49c86a52ca51976d75807dc6366fdde2b
ming-der wang <mingderwang@me.com>
2025-04-03 16:52:45 +0800

    ok for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	}
+  output: 'server', // or 'hybrid' if mixing static and SSR
+  adapter: node({
+    mode: 'standalone' // or 'middleware' depending on your use case
+  }),
 });
